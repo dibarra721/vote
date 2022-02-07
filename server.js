e0d0a29e9d7a980033f2a94b3f0c7c3689a98774
Diana I <89826035+dibarra721@users.noreply.github.com>
2022-02-07 12:00:35 -0700

    initial commit

--- server.js
+++ server.js
@@ -4,14 +4,30 @@ require("dotenv").config()
 const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressjwt = require("express-jwt")
+const secret = process.env.SECRET || "Bobs Burger"
 
-process.env.SECRET
+
+
+
+
+const PORT = process.env.PORT || 9000;
+// ... other imports
+const path = require("path")
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+
+
 
 app.use(express.json())
 app.use(morgan('dev'))
 
 mongoose.connect(
   'mongodb://localhost:27017/rockthevotedb',
+  {
+    useNewUrlParser: true,
+   
+  },
+
   () => console.log('Connected to the rtv DB')
 )
 
@@ -26,6 +42,14 @@ app.use((err, req, res, next) => {
   return res.send({errMsg: err.message})
 })
 
-app.listen(9000, () => {
+
+// Right before your app.listen(), add this:
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+
+
+app.listen(PORT, () => {
   console.log(`Server is running on local port 9000`)
 })
